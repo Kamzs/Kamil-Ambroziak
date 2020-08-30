f22717c35eb22aa381db77004b5cf517aac304f6
Kamzs <kamilambroziak@interia.pl>
2020-08-30 17:08:01 +0200

    refactor

--- worker/worker.go
+++ worker/worker.go
@@ -2,6 +2,7 @@ package worker
 
 import (
 	fetchers "Kamil-Ambroziak"
+	"Kamil-Ambroziak/logger"
 	"Kamil-Ambroziak/storage"
 	"Kamil-Ambroziak/utils"
 	"context"
@@ -18,7 +19,6 @@ var storageCli fetchers.Storage
 
 func init() {
 	storageCli, _ = storage.NewMySQL()
-
 }
 
 func NewWorker() *worker {
@@ -49,49 +49,27 @@ func doJob(fetcher *fetchers.Fetcher) {
 	defer cancel()
 	req, err := http.NewRequest("GET", fetcher.Url, nil)
 	if err != nil {
-		fmt.Println("error request")
+		logger.Error(fmt.Sprintf("error when trying prepare request for job %v",fetcher.JobID), err)
 		return
 	}
-	//var start, connect, dns, tlsHandshake time.Time
-	trace := &httptrace.ClientTrace{
-/*		DNSStart: func(dsi httptrace.DNSStartInfo) { dns = time.Now() },
-		DNSDone: func(ddi httptrace.DNSDoneInfo) {
-			fmt.Printf("DNS Done: %v\n", time.Since(dns))
-		},
-
-		TLSHandshakeStart: func() { tlsHandshake = time.Now() },
-		TLSHandshakeDone: func(cs tls.ConnectionState, err error) {
-			fmt.Printf("TLS Handshake: %v\n", time.Since(tlsHandshake))
-		},
-
-		ConnectStart: func(network, addr string) { connect = time.Now() },
-		ConnectDone: func(network, addr string, err error) {
-			fmt.Printf("Connect time: %v\n", time.Since(connect))
-		},
-
-		GotFirstResponseByte: func() {
-			fmt.Printf("Time from start to first byte: %v\n", time.Since(start))
-		},*/
-	}
+	trace := &httptrace.ClientTrace{}
 	ctxTimer := httptrace.WithClientTrace(ctxTimeout, trace)
 
 	historyEl := &fetchers.HistoryElement{
 		Id:        fetcher.Id,
 		CreatedAt: time.Now().Unix(),
 	}
-	//res, err := http.DefaultClient.Do(req.WithContext(ctxTimer))
 	start := time.Now()
 	res, err := http.DefaultTransport.RoundTrip(req.WithContext(ctxTimer))
-
 	totalResponseTime := time.Since(start)
 	if err != nil {
 		if fmt.Sprintf("%T", err) == "context.deadlineExceededError" {
 			historyEl.Response = ""
 			historyEl.Duration = 5
-			_ = storageCli.SaveHistoryForFetcher(historyEl)
+			err = storageCli.SaveHistoryForFetcher(historyEl)
+			logger.Error(fmt.Sprintf("history for fetcher %v could not be saved",fetcher.Id), err)
 		}
-		//todo add logger
-
+		logger.Error(fmt.Sprintf("page %s could not be raeched",fetcher.Url), err)
 	} else {
 		if res.StatusCode == http.StatusOK {
 			bodyBytes, err := ioutil.ReadAll(res.Body)
@@ -100,13 +78,9 @@ func doJob(fetcher *fetchers.Fetcher) {
 				return
 			}
 			historyEl.Response = string(bodyBytes)
-			/*			nanoseconds := totalResponseTime - start
-						//duration := time.Duration(nanosecods)
-						duration := float64(nanoseconds)/float64(time.Second)*/
-			/*			fmt.Println(duration)
-						fmt.Printf("%.2f", duration)*/
 			historyEl.Duration = totalResponseTime.Seconds()
 		}
-		_ = storageCli.SaveHistoryForFetcher(historyEl)
+		err = storageCli.SaveHistoryForFetcher(historyEl)
+		logger.Error(fmt.Sprintf("history for fetcher %v could not be saved",fetcher.Id), err)
 	}
 }
