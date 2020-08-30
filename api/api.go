aaa7b72f6aa93f56c4352b5baa1d43dd2640289e
Kamzs <kamilambroziak@interia.pl>
2020-08-30 02:03:28 +0200

    added middleware checking size of payload

--- api/api.go
+++ api/api.go
@@ -2,7 +2,9 @@ package api
 
 import (
 	fetchers "Kamil-Ambroziak"
+	"Kamil-Ambroziak/utils"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type Api struct{
@@ -16,10 +18,20 @@ func NewAPIServer(mySqlClient fetchers.Storage, worker fetchers.Worker) {
 		Worker: worker,
 	}
 	router := gin.Default()
+	router.Use(checkSize)
 	router.POST("/api/fetcher", api.AddFetcher)
 	router.PATCH("/api/fetcher/:id", api.UpdateFetcher)
 	router.DELETE("/api/fetcher/:id", api.DeleteFetcher)
 	router.GET("/api/fetcher", api.GetAllFetchers)
 	router.GET("/api/fetcher/:id/history", api.GetHistoryForFetcher)
 	router.Run(":8080")
+}
+
+func checkSize(c *gin.Context){
+	size := c.Request.ContentLength
+	if size > 10 {
+		c.AbortWithStatusJSON(http.StatusRequestEntityTooLarge,utils.NewRestError("entity too large",http.StatusRequestEntityTooLarge,"payload can be max 1024 bytes",nil))
+		return
+	}
+	c.Next()
 }
